d4e7d3509d1485f217fb55f7cb0470c7a96d7041
Vickouma77 <Vickprestone20@gmail.com>
2025-09-12 22:37:31 +0300

    chore: remove generated target files and cache directories

--- target/debug/deps/tutor_nodb-dc2c4881e1ed06a1.d
+++
@@ -1,5 +0,0 @@
-/home/vick/ezytutors/target/debug/deps/tutor_nodb-dc2c4881e1ed06a1.d: tutor-nodb/src/main.rs
-
-/home/vick/ezytutors/target/debug/deps/libtutor_nodb-dc2c4881e1ed06a1.rmeta: tutor-nodb/src/main.rs
-
-tutor-nodb/src/main.rs:
