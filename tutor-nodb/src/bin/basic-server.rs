6a33a1df7ebd16f8464a6c4f0d47afa190b236db
Vickouma77 <Vickprestone20@gmail.com>
2025-09-12 23:12:53 +0300

    feat(basic-server): implement basic server with health check route
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- tutor-nodb/src/bin/basic-server.rs
+++ tutor-nodb/src/bin/basic-server.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+fn general_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check_handler));
+}
+
+// the handler constructs http response
+async fn health_check_handler() -> impl Responder {
+    HttpResponse::Ok().json("Health check OK")
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(general_routes)
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
+}
