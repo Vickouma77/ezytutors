9eadd2330be49abb685127ce502e090de6346da4
Vickouma77 <Vickprestone20@gmail.com>
2025-09-12 22:20:50 +0300

    Initialize project structure with Cargo.toml and main.rs files

---
+++ target/debug/deps/tutor_nodb-c24f7cccac5e5071.d
@@ -0,0 +1,5 @@
+/home/vick/ezytutors/target/debug/deps/tutor_nodb-c24f7cccac5e5071.d: tutor-nodb/src/main.rs
+
+/home/vick/ezytutors/target/debug/deps/libtutor_nodb-c24f7cccac5e5071.rmeta: tutor-nodb/src/main.rs
+
+tutor-nodb/src/main.rs:
