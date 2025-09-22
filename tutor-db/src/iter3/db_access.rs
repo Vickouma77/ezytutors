b5211d44de432cc92b291490f6a7478fc37ee0bb
Vickouma77 <Vickprestone20@gmail.com>
2025-09-22 18:47:04 +0300

    feat(iter3): add get_course_details_db function to retrieve course details for a tutor

--- tutor-db/src/iter3/db_access.rs
+++ tutor-db/src/iter3/db_access.rs
@@ -1,4 +1,5 @@
 // Single Responsibility Principle
+
 use sqlx::postgres::PgPool;
 use crate::iter3::Course;
 
@@ -26,4 +27,25 @@ pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Cours
             .into()),
         })
         .collect()
+}
+
+pub async fn get_course_details_db(pool: &PgPool, tutor_id: i32, course_id: i32) -> Course{
+    // SQL statement
+    let course_row = sqlx::query!(
+        "SELECT tutor_id, course_id, course_name, posted_time FROM
+            ezy_course_c4 where tutor_id = $1 and course_id = $2",
+            tutor_id, course_id
+    )
+    .fetch_one(pool)
+    .await
+    .unwrap();
+
+    Course {
+        course_id: course_row.course_id,
+        tutor_id: course_row.tutor_id,
+        course_name: course_row.course_name,
+        posted_time: Some(chrono::NaiveDateTime::from(
+            course_row.posted_time.unwrap()
+        ).into()),
+    }
 }
