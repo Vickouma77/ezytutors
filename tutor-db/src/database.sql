87120414519f4a29346e2572ba42ff1f64abee67
Vickouma77 <Vickprestone20@gmail.com>
2025-09-23 10:04:43 +0300

    fix(database): correct table name and update seed data timestamps for ezy_course_c5

--- tutor-db/src/database.sql
+++ tutor-db/src/database.sql
@@ -1,7 +1,7 @@
 /* Drop table if it already exists*/
-drop table if exists ezy_course_c4;
+drop table if exists ezy_course_c5;
 /* Create a table. */
-create table ezy_course_c4
+create table ezy_course_c5
 (
     course_id serial primary key,
     tutor_id INT not null,
@@ -9,10 +9,10 @@ create table ezy_course_c4
     posted_time TIMESTAMP default now()
 );
 /* Load seed data for testing */
-insert into ezy_course_c4
+insert into ezy_course_c5
     (course_id,tutor_id, course_name,posted_time)
-values(1, 1, 'First course', '2020-12-17 05:40:00');
+values(1, 1, 'First course', '2025-09-23 05:40:00');
 
-insert into ezy_course_c4
+insert into ezy_course_c5
     (course_id, tutor_id, course_name,posted_time)
-values(2, 1, 'Second course', '2020-12-18 05:45:00');
+values(2, 1, 'Second course', '2025-09-23 05:45:00');
