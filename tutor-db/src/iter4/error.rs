25d494b73cfbb46121ed530b2eb3415e1fd077fa
Vickouma77 <Vickprestone20@gmail.com>
2025-09-23 14:55:39 +0300

    feat(iter4): implement custom error handling with EzytutoError enum and MyErrorResponse struct

--- tutor-db/src/iter4/error.rs
+++ tutor-db/src/iter4/error.rs
@@ -1,1 +1,61 @@
+use actix_web::{error, http::StatusCode, HttpResponse};
+use serde::{Deserialize, Serialize};
+use log::error;
 
+#[derive(Debug, Deserialize, Serialize)]
+pub enum EzytutorError {
+    DBError(String),
+    ActixError(String),
+    NotFound(String),
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MyErrorResponse {
+    error_message: String,
+}
+
+impl std::fmt::Display for EzytutorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EzytutorError::DBError(msg) => write!(f, "Database error: {}", msg),
+            EzytutorError::ActixError(msg) => write!(f, "Internal server error: {}", msg),
+            EzytutorError::NotFound(msg) => write!(f, "Not found: {}", msg),
+        }
+    }
+}
+
+impl EzytutorError {
+    pub fn error_response(&self) -> String {
+        match self {
+            EzytutorError::DBError(msg) => {
+                error!("Database Error occurred {:?}", msg);
+                "Database error".into()
+            }
+            EzytutorError::ActixError(msg) => {
+                error!("Server Error occurred {:?}", msg);
+                "Internal server error".into()
+            }
+            EzytutorError::NotFound(msg) => {
+                error!("Not Found Error occurred {:?}", msg);
+                msg.into()
+            }
+        }
+    }
+}
+
+impl error::ResponseError for EzytutorError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            EzytutorError::DBError(_) | EzytutorError::ActixError(_) => {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+            EzytutorError::NotFound(_) => StatusCode::NOT_FOUND
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code()).json(MyErrorResponse {
+            error_message: self.to_string(),
+        })
+    }
+}
