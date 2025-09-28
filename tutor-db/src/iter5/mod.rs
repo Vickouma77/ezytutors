1c3b86a1178069bbcd8873901d8acc1dddea0c04
Vickouma77 <Vickprestone20@gmail.com>
2025-09-28 22:22:45 +0300

    feat(iter5): initialize module structure for iter5 with empty files

---
+++ tutor-db/src/iter5/mod.rs
@@ -0,0 +1,13 @@
+mod errors;
+mod dbaccess;
+mod handlers;
+mod models;
+mod routes;
+mod state;
+
+pub use errors::*;
+pub use dbaccess::*;
+pub use handlers::*;
+pub use models::*;
+pub use routes::*;
+pub use state::*;
