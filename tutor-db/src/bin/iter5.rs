76823fea10389b7c24b6a0e2033ef6d7161514b4
Vickouma77 <Vickprestone20@gmail.com>
2025-09-28 22:34:44 +0300

    feat(iter5): implement main function for HTTP server with database connection

--- tutor-db/src/bin/iter5.rs
+++ tutor-db/src/bin/iter5.rs
@@ -1,4 +1,36 @@
+use std::{env, io, sync::Mutex};
+
+use actix_web::{App, HttpServer, middleware::Logger, web};
+use dotenv::dotenv;
+use sqlx::PgPool;
+use tutor_db::iter4::{AppState, course_routes, general_routes};
+
 #[actix_rt::main]
-async fn main() {
-    
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    env_logger::init();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE URL is not set");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+
+    let host_port = env::var("HOST_PORT").expect("HOST PORT is not set");
+    let bind_address = format!("127.0.0.1:{}", host_port);
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    })
+    .bind(bind_address)?
+    .run()
+    .await
 }
