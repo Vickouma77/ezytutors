5fecc21cec4df4bdac74f437ea87ae30f8fcbe10
Vickouma77 <Vickprestone20@gmail.com>
2025-10-04 20:32:23 +0300

    feat: define Tutor struct with serialization support

--- tutor-db/src/iter5/models/tutor.rs
+++ tutor-db/src/iter5/models/tutor.rs
@@ -1,1 +1,10 @@
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Tutor {
+    tutor_id: i32,
+    tutor_name: String,
+    tutor_pic_url: String,
+    tutor_profile: String
+}
