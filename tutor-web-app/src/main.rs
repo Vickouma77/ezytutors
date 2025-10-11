3f03abf7ae6b236415634570ebec91cea37a00be
Vickouma77 <Vickprestone20@gmail.com>
2025-10-11 22:15:57 +0300

    feat: add actix-files dependency and implement static file serving in the tutor web app

--- tutor-web-app/src/main.rs
+++ tutor-web-app/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:8000".to_string());
+    println!("Listening on: {}, open browser and visit have a try!",addr);
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/static", "./static").show_files_listing())
+    })
+    .bind(addr)?
+    .run()
+    .await
 }
